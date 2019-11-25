496767e60b9e84312a80fa3b0868c1bc600edbce
PajGUN <pajgun13@gmail.com>
2019-11-25 21:46:50 +0300

    semaphore task complete

--- src/main/java/concurrency/prodcons/ProducerConsumer.java
+++ src/main/java/concurrency/prodcons/ProducerConsumer.java
@@ -1,33 +1,33 @@
 package concurrency.prodcons;
 
+import java.util.concurrent.Semaphore;
+
 public class ProducerConsumer {
 
     private int count = 0;
+    private Semaphore semaphore = new Semaphore(1);
+
 
     public void produce() throws InterruptedException {
         while (true) {
-            synchronized (this) {
-                 while (count > 9) {
-                    wait();
-                }
+            semaphore.acquire();
+            while (count <= 9){
                 System.out.println(Thread.currentThread().getName() + " " +  count++ + " ");
                 Thread.sleep(500);
-                notify();
             }
+            semaphore.release();
         }
     }
 
 
     public void consume() throws InterruptedException {
         while (true) {
-            synchronized (this) {
-                while (count <= 1) {
-                    wait();
+            semaphore.acquire();
+                while (count >= 1) {
+                    System.out.println(Thread.currentThread().getName() + " "+ count-- );
+                    Thread.sleep(500);
                 }
-                System.out.println(Thread.currentThread().getName() + " "+ count-- );
-                Thread.sleep(500);
-                notify();
-            }
+            semaphore.release();
         }
     }
 }
